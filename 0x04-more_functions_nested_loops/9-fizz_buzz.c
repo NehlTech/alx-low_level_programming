9aeca5a34918bce3f2304d52ec671ba48414a275
bright <aduboahenebright@gmail.com>
2022-04-14 18:01:15 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,43 +9,41 @@
 
 int main(void)
 {
+
+	int p = 100;
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= p)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-	
-		{
-
-			printf(" FizzBuzz");
-
-		} else if (i == 1)
-
-
-
-		{
-
-			printf("%d", i);
-
-		} else
-
-		{
-
-			printf(" %d", i);
-
+			printf("FizzBuzz ");
 		}
 
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (i % 5 == 0)
+		{
+			if (i < p)
+				printf("Buzz ");
+
+			else
+				printf("Buzz");
+		}
+
+		else
+		{
+			printf("%i ", i);
+		}
+
+		i++;
+
+
 	}
-
 	printf("\n");
-
-
 	return (0);
-
 }
