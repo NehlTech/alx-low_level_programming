07336cdbc72c2d4e47b1d94461742752c144b9cb
bright <aduboahenebright@gmail.com>
2022-04-26 11:28:26 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,8 +4,7 @@
   * _strchr - Locates a character in a string
   * @s: String
   * @c: Character to search
-  * Return: Pointer to the first occurrence of the character c in the string s
-  **/
+  * Return: Pointer to the first occurrence of the character c in the string  **/
 
 char *_strchr(char *s, char c)
 {
