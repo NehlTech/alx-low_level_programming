73753263066f1f34a23493840eadf19f7ce8a4e4
NehlTech <obololastkiller@gmail.com>
2022-07-15 01:49:43 +0000

    Alx

--- 0x18-dynamic_libraries/tests/0-main.c
+++ 0x18-dynamic_libraries/tests/0-main.c
@@ -1,13 +1,13 @@
-#include "../holberton.h"
+#include "../main.h"
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for Alx students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-	printf("%d\n", _strlen("Holberton"));
+	printf("%d\n", _strlen("ALX SE"));
 	return (EXIT_SUCCESS);
 }
