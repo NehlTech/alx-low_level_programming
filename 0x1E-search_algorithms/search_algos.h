46613d9792c06b74989b1f7686ae0f0e477c280b
NehlTech <obololastkiller@gmail.com>
2022-11-03 04:41:34 +0000

    Search Algo Header File

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,19 +1,13 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_recursion(int *array, int value, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* SEARCH_ALGOS_H */
+#endif
